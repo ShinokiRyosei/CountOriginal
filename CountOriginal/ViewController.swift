a52c12433aa22da00a664ce2e7eb261f8b3f02b8
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-06-12 22:16:02 +0900

    Update Swift3 syntax

--- CountOriginal/ViewController.swift
+++ CountOriginal/ViewController.swift
@@ -10,53 +10,47 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController, AVSpeechSynthesizerDelegate {
-    
+
     @IBOutlet var label: UILabel!
     var number: Int = 0
-    var synthesizer = AVSpeechSynthesizer()
+    let synthesizer = AVSpeechSynthesizer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         synthesizer.delegate = self
-//        synthesizer.accessibilityActivate()
-        
+        //        synthesizer.accessibilityActivate()
+
         do{
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-            
+
             do{
                 try AVAudioSession.sharedInstance().setActive(true)
             }catch{
-                
+
             }
         }catch{
-            
+
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     @IBAction func plus () {
-       number = number + 1
+        number = number + 1
         label.text = String(number)
-        
+
         let voiceText = AVSpeechUtterance(string: String(number))
         voiceText.voice = AVSpeechSynthesisVoice(language: "en_US")
-        self.synthesizer.speakUtterance(voiceText)
+        self.synthesizer.speak(voiceText)
     }
-    
+
     @IBAction func minus() {
         number = number - 1
         label.text = String(number)
-        
+
         let voiceText = AVSpeechUtterance(string: String(number))
         voiceText.voice = AVSpeechSynthesisVoice(language: "ja_JP")
-        self.synthesizer.speakUtterance(voiceText)
+        self.synthesizer.speak(voiceText)
     }
-
-
+    
+    
 }
 
